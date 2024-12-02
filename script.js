58600b96cb6a80ae1d5d6cd6590d72d4dc610402
Rotimi-slack <arowolo96@gmail.com>
2024-12-02 07:17:51 +0100

    added FAq section

--- script.js
+++ script.js
@@ -767,6 +767,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
+document.querySelectorAll('.faq-question').forEach((button) => {
+    button.addEventListener('click', () => {
+        const faqItem = button.parentElement;
+        faqItem.classList.toggle('open');
+    });
+});
 
 
 
